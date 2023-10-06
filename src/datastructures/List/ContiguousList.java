7d82a6f466d92976d73e2082edbbfe6d38858556
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2023-10-06 21:25:34 +0530

    add list

--- src/datastructures/List/ContiguousList.java
+++ src/datastructures/List/ContiguousList.java
@@ -1,4 +1,104 @@
 package datastructures.List;
 
 public class ContiguousList {
+    private int maxSize;
+    private int position;
+    private int[] list;
+
+    public ContiguousList(int size){
+        maxSize = size;
+        list = new int[maxSize];
+        position=-1;
+    }
+
+    // check if list is empty
+    public boolean isListEmpty(){
+        return (position == -1);
+    }
+
+    // check if list is full
+    public boolean isListFull(){
+        return (position == maxSize - 1);
+    }
+
+    // return the list size at that moment
+    public int listSize(){
+        return (position + 1);
+    }
+
+    // insert an element at the end of the list
+    public void insertLast(int value){
+        if (isListFull()) {
+            System.out.println("List is full. Can't insert an element to a full list.");
+        } else {
+            list[++position] = value;
+        }
+    }
+
+    // insert an element to the list for a given index;
+    public void insertList(int value, int index){
+        int i;
+        if (isListFull()){
+            System.out.println("List is full. Can't insert an element to a full list.");
+        } else if (index < 0 || index > listSize()) {
+            System.out.println("Invalid index. Can't insert the element to given index.");
+        } else {
+            for (i = listSize(); i > index; i--) {
+                list[i] = list[i-1];
+            }
+            list[index] = value;
+            position++;
+        }
+    }
+
+    // delete an element from the given index
+    public int deleteList(int index){
+        int i, element;
+        if (isListEmpty()){
+            System.out.println("List is empty. Can't delete an element from a empty list.");
+        } else if (index < 0 || index >= listSize()) {
+            System.out.println("Invalid index. Can't delete the element at given index.");
+        } else {
+            element = list[index];
+            for (i = index; i < listSize() - 1; i++) {
+                list[i] = list[i+1];
+            }
+            position--;
+            return element;
+        }
+        return 0;
+    }
+
+    // show an element from the given index
+    public int retrieveList(int index){
+        int element;
+        if (isListEmpty()){
+            System.out.println("List is empty. Can't retrieve an element from a empty list.");
+        } else if (index < 0 || index >= listSize()) {
+            System.out.println("Invalid index. Can't delete the element at given index.");
+        } else {
+            element = list[index];
+            return element;
+        }
+        return 0;
+    }
+
+    // replace an element in the list with a given index and the value
+    public void replaceList(int index, int value){
+        if (isListEmpty()){
+            System.out.println("List is empty. Can't replace an element from a empty list.");
+        } else if (index < 0 || index >= listSize()){
+            System.out.println("Invalid index. Can't replace the element at given index.");
+        } else {
+            list[index] = value;
+        }
+    }
+
+    // print all elements in the list
+    public void traversalList(){
+        int i;
+        for (i = 0; i < position - 1 ; i++) {
+            System.out.println(list[i]);
+        }
+    }
 }
--- src/datastructures/List/LinkedList.java
+++ src/datastructures/List/LinkedList.java
@@ -1,4 +1,96 @@
 package datastructures.List;
 
+// node class
+class Node {
+    int data;
+    Node next;
+}
+
 public class LinkedList {
+    private Node head;
+    private int count;
+
+    public LinkedList(){
+        head = null;
+        count = 0;
+    }
+
+    // check if list is empty
+    public boolean isListEmpty(){
+        return (head == null);
+    }
+
+    // return the list size at that moment
+    public int listSize(){
+        return count;
+    }
+
+    // insert an element at the end of the list
+    public void insertLast(int value){
+        Node node = new Node();
+        node.data = value;
+        node.next = null;
+        if (head == null) {
+            head = node;
+        } else {
+            Node n = head;
+            while (n.next != null) {
+                n = n.next;
+            }
+            n.next = node;
+        }
+        count++;
+    }
+
+    // insert an element to the list for a given index;
+    public void insertList(int index, int value){
+        Node node = new Node();
+        node.data = value;
+        node.next = null;
+
+        if (index < 0 || index > listSize()){
+            System.out.println("Invalid index. Can't insert the element to given index.");
+        } else {
+            Node n = head;
+            for (int i = 0; i < index - 1; i++){
+                n = n.next;
+            }
+            node.next = n.next;
+            n.next = node;
+            count++;
+        }
+    }
+
+    // delete an element from the given index
+    public void deleteList(int index){
+        if (isListEmpty()){
+            System.out.println("List is empty. Can't delete an element from a empty list.");
+        } else {
+            if (index < 0 || index > listSize()) {
+                System.out.println("Invalid index. Can't delete the element at given index.");
+            } else if (index == 0) {
+                head = head.next;
+            } else {
+                Node n1 = head;
+                Node n2 = null;
+                for (int i = 0; i < index - 1; i++){
+                    n1 = n1.next;
+                }
+                n2 = n1.next;
+                n1.next = n2.next;
+                n2 = null;
+            }
+            count--;
+        }
+    }
+
+    // print all elements in the list
+    public void traversalList(){
+        Node node = head;
+        while (node.next != null){
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
 }
