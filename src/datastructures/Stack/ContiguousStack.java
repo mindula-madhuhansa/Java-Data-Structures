770a2a9a44e13a139a00307fb04130404dff2ccf
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2023-10-07 01:14:01 +0530

    add stack

--- src/datastructures/Stack/ContiguousStack.java
+++ src/datastructures/Stack/ContiguousStack.java
@@ -1,4 +1,42 @@
 package datastructures.Stack;
 
 public class ContiguousStack {
+    private int top;
+    private int maxSize;
+    private int[] stack;
+
+    public ContiguousStack(int size){
+        maxSize = size;
+        stack = new int[maxSize];
+        top = -1;
+    }
+
+    // check if stack is empty
+    public boolean isStackEmpty(){
+        return (top == -1);
+    }
+
+    // check if stack is full
+    public boolean isStackFull(){
+        return (top == maxSize - 1);
+    }
+
+    // push an element to top of the stack
+    public void push(int element){
+        if (isStackFull()){
+            System.out.println("Stack is full.");
+            System.exit(1);
+        } else {
+            stack[++top] = element;
+        }
+    }
+
+    // pop last element from the stack
+    public int pop(){
+        if (isStackEmpty()) {
+            System.out.println("Stack is empty");
+            System.exit(1);
+        }
+        return (stack[top--]);
+    }
 }
--- src/datastructures/Stack/LinkedStack.java
+++ src/datastructures/Stack/LinkedStack.java
@@ -1,4 +1,56 @@
 package datastructures.Stack;
 
+// node class
+class Node {
+    int value;
+    Node next;
+}
 public class LinkedStack {
+    private Node top;
+    private int size;
+
+    public LinkedStack(){
+        this.top = null;
+        this.size = 0;
+    }
+
+    // check if stack is empty
+    public boolean isStackEmpty(){
+        return (top == null);
+    }
+
+    // print the top element of the stack
+    public int peak(){
+        if (isStackEmpty()){
+            System.out.println("Stack is empty");
+        }
+        return top.value;
+    }
+
+    // remove the top element of the stack
+    public int pop(){
+        if (top == null) {
+            System.out.println("Stack is empty");
+            return 0;
+        } else {
+            int element = top.value;
+            this.size -= 1;
+            this.top = this.top.next;
+            return element;
+        }
+    }
+
+    // add an element to the top of the stack
+    public void push(int element){
+        Node node = new Node();
+        if (node == null){
+            System.out.println("Heap overflow");
+            System.exit(1);
+        } else {
+            node.value = element;
+            node.next = top;
+            top = node;
+            this.size += 1;
+        }
+    }
 }
